790e62c0fde0c7ef02cc70a08dbe8bca3fb6e293
康博三好 <hdfnm774@yahoo.co.jp>
2019-08-15 07:33:50 +0000

    課題です

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -9,21 +9,22 @@ class ProfileController extends Controller
 {
     //
     public function add()
-  {
-      return view('admin.profile.create');
-  }
+      {
+          return view('admin.profile.create');
+      }
 
-  public function create()
-  {
-      return redirect('admin/profile/create');
-  }
+    public function create()
+      {
+          return redirect('admin/profile/create');
+      }
 
- public function edit()
- {
- return view('admin.profile.edit');
- }
- public function update()
- {
- return redirect('admin/profile/edit');
- }
+    public function edit()
+     {
+            return view('admin.profile.edit');
+     }
+   
+    public function update()
+     {
+            return redirect('admin/profile/edit');
+     }
 }
