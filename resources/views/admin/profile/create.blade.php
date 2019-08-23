ae70698238b537773e4cb874f7f1cb924ad391f4
康博三好 <hdfnm774@yahoo.co.jp>
2019-08-23 14:33:34 +0000

    PHP/Laravel 13課題

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,17 +1,57 @@
-{{-- layouts/profile.blade.phpを読み込む --}}
 @extends('layouts.profile')
-
-
-{{-- profile.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
 @section('title', 'My プロフィール')
 
-{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>My　プロフィール</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    
+                    <div class="form-group row">
+                        <label class="col-md-2" for="name">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                        </div>
+                    </div>
+ 
+                     <div class="form-group row">
+                        <label class="col-md-2" for="gender">性別</label>
+                        <div class="col-md-10">
+                            <input type="radio" class="form-control" name="male" value="男">男
+                            <input type="radio" class="form-control" name="female" value="女">女
+                        </div>
+                    </div>
+                    
+                    <div class="form-group row">
+                        <label class="col-md-2" for="hobby">趣味</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="hobby" value="{{ old('hobby') }}">
+                        </div>
+                    </div>
+                    
+                    <div class="form-group row">
+                        <label class="col-md-2" for="body">自己紹介欄</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
+                        </div>
+                    </div>
+                    
+
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
             </div>
         </div>
     </div>
 @endsection
+
+
--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,16 @@ Route::group(['prefix' => 'admin'], function() {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+     Route::get('news/create', 'Admin\NewsController@add');
+     Route::post('news/create', 'Admin\NewsController@create');
+});
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::post('profile/create', 'Admin\ProfileController@create')->middleware('auth');
+});
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::post('profile/edit', 'Admin\ProfileController@updata')->middleware('auth');
+});
