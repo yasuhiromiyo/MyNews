85c5277338f6d64c6c77de6998827086ebe51141
康博三好 <hdfnm774@yahoo.co.jp>
2019-08-27 07:42:03 +0000

    PHP/Laravel 14課題

---
+++ app/News.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    protected $guarded = array('id');
+
+    // 以下を追記
+    public static $rules = array(
+        'title' => 'required',
+        'body' => 'required',
+        'name' => 'required',
+        'gender' => 'required',
+        'hobby' => 'required',
+        'introduction' => 'required',
+    );
+}
